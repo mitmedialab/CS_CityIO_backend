5f57fcdb00cd2e164bde444a5649263373c74198
Yasushi Sakai <ysshski@gmail.com>
2017-10-06 13:47:10 +0000

    initial commit for diastrofismo

--- src/controllers/ApiController.ts
+++ src/controllers/ApiController.ts
@@ -12,76 +12,25 @@ const router: Router = Router()
 router.get('/', async (req: Request, res: Response) => {
   const tableNames = tableManager.getList()
 
-  const links = tableNames.reduce((result, tn) => {
-    return result + `<li><a href="${baseURL}/api/table/${tn}">${tn}</a></li>`
-  }, '')
-
   res.send(html(`
-    <img src="http://www.vgmuseum.com/end/nes/a/venuswar-3.png" width="512">
-    <p>available tables and json</p>
-    <ul>
-    ${links}
-    </ul>
-    <p> more on api and documenation for the server:
-      <a href="https://github.com/mitmedialab/cityioserver">github repository</a>
-    </p>
-    `))
+    <p>diastrofismo</p>
+        `))
 })
 
 /*
- * get table data
+ * send new pixel
  * */
-router.get('/table/:tableName', async (req: Request, res: Response) => {
-  const tableName: string = req.params.tableName
-  const table: ITable = await tableManager.getTable(tableName)
-  res.json(table)
+router.get('/image/send/:value', async (req: Request, res: Response) => {
+  const value: number  = +req.params.value
+  res.json(value)
 })
 
 /*
- * post table data
+ * get image data
  * TODO: get rid of update
  * */
-router.post('/table/update/:tableName', async (req: Request, res: Response) => {
-  const tableName: string = req.params.tableName
-  let tableData: any
-
-  switch (req.headers['content-type']) {
-    case 'application/json' :
-      tableData = req.body
-      break
-    case 'text/plain' :
-    default :
-      try {
-        tableData = JSON.parse(req.body.toString('utf8'))
-      } catch (e) {
-        if (e instanceof SyntaxError) {
-          res.status(500)
-            .send(`
-            <h1> Invalid JSON </h1>
-            <p> check data </p>
-            <p> ${e.name}: ${e.message} </p>
-              `)
-          return
-        }
-      }
-      break
-  }
-  // the table data does not contain the id yet.
-  const formattedTableData: ITable = {...emptyTable, ...tableData, timestamp: Date.now()}
-  // console.log(formattedTableData)
-  const newTable: ITable = await tableManager.addTable(tableName, formattedTableData)
-
-  res.json(newTable)
-})
-
-/*
- * clear table data
- * */
-router.get('/table/clear/:tableName', async (req: Request, res: Response) => {
-  const tableName: string = req.params.tableName
-  await tableManager.clearTable(tableName)
-  res.json(`cleared ${tableName}`)
-
-})
+router.get('/image/current.png', async (req: Request, res: Response) => {
+  res.json(gives you a image')
+  })
 
 export const ApiController: Router = router
